060ac2eb077086ef48db44a6719d11b26e2ad6a2
Kevin Deng <31214837+kevindjf66@users.noreply.github.com>
2017-09-14 09:54:11 -0400

    Add files via upload

---
+++ hw03_sol.py
@@ -0,0 +1,300 @@
+HW_SOURCE_FILE = 'hw03.py'
+
+#############
+# Questions #
+#############
+
+def g(n):
+    """Return the value of G(n), computed recursively.
+
+    >>> g(1)
+    1
+    >>> g(2)
+    2
+    >>> g(3)
+    3
+    >>> g(4)
+    10
+    >>> g(5)
+    22
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'g', ['While', 'For'])
+    True
+    """
+    if n in (1, 2, 3):
+        return n
+    return g(n-1) + 2*g(n-2) + 3*g(n-3)
+
+def g_iter(n):
+    """Return the value of G(n), computed iteratively.
+
+    >>> g_iter(1)
+    1
+    >>> g_iter(2)
+    2
+    >>> g_iter(3)
+    3
+    >>> g_iter(4)
+    10
+    >>> g_iter(5)
+    22
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'g_iter', ['Recursion'])
+    True
+    """
+    if n == 1 or n == 2 or n == 3:
+        return n
+    a, b, c = 1, 2, 3
+    while n > 3:
+        a, b, c = b, c, c + 2*b + 3*a
+        n = n - 1
+    return c
+
+def has_seven(k):
+    """Returns True if at least one of the digits of k is a 7, False otherwise.
+
+    >>> has_seven(3)
+    False
+    >>> has_seven(7)
+    True
+    >>> has_seven(2734)
+    True
+    >>> has_seven(2634)
+    False
+    >>> has_seven(734)
+    True
+    >>> has_seven(7777)
+    True
+    """
+    if k % 10 == 7:
+        return True
+    elif k < 10:
+        return False
+    else:
+        return has_seven(k // 10)
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    7
+    >>> pingpong(8)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    0
+    >>> pingpong(30)
+    6
+    >>> pingpong(68)
+    2
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    1
+    >>> pingpong(72)
+    0
+    >>> pingpong(100)
+    2
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    def pingpong_next(k, p, up):
+        if k == n:
+            return p
+        if up:
+            return pingpong_switch(k+1, p+1, up)
+        else:
+            return pingpong_switch(k+1, p-1, up)
+
+    def pingpong_switch(k, p, up):
+        if k % 7 == 0 or has_seven(k):
+            return pingpong_next(k, p, not up)
+        else:
+            return pingpong_next(k, p, up)
+
+    return pingpong_next(1, 1, True)
+
+# Alternate solution
+def pingpong2(n):
+    if n <= 7:
+        return n
+    return direction(n) + pingpong(n-1)
+
+def direction(n):
+    if n < 7:
+        return 1
+    if (n-1) % 7 == 0 or has_seven(n-1):
+        return -1 * direction(n-1)
+    return direction(n-1)
+
+def count_change(amount):
+    """Return the number of ways to make change for amount.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    """
+    return count_using(1, amount)
+
+def count_using(min_coin, amount):
+    if amount < 0:
+        return 0
+    elif amount == 0:
+        return 1
+    elif min_coin > amount:
+        return 0
+    else:
+        with_min = count_using(min_coin, amount - min_coin)
+        without_min = count_using(2*min_coin, amount)
+        return with_min + without_min
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+def move_stack(n, start, end):
+    """Print the moves required to move n disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    n -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least n disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top n start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    if n == 1:
+        print_move(start, end)
+    else:
+        other = 6 - start - end
+        move_stack(n-1, start, other)
+        print_move(start, end)
+        move_stack(n-1, other, end)
+
+def flatten(lst):
+    """Returns a flattened version of lst.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]      # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    """
+    if not lst:
+        return []
+    elif type(lst[0]) == list:
+        return flatten(lst[0]) + flatten(lst[1:])
+    else:
+        return [lst[0]] + flatten(lst[1:])
+
+def merge(lst1, lst2):
+    """Merges two sorted lists.
+
+    >>> merge([1, 3, 5], [2, 4, 6])
+    [1, 2, 3, 4, 5, 6]
+    >>> merge([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge([1, 2, 3], [])
+    [1, 2, 3]
+    >>> merge([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    """
+    # recursive
+    if not lst1 or not lst2:
+        return lst1 + lst2
+    elif lst1[0] < lst2[0]:
+        return [lst1[0]] + merge(lst1[1:], lst2)
+    else:
+        return [lst2[0]] + merge(lst1, lst2[1:])
+
+# Iterative solution 
+def merge_iter(lst1, lst2):
+    """Merges two sorted lists.
+
+    >>> merge_iter([1, 3, 5], [2, 4, 6])
+    [1, 2, 3, 4, 5, 6]
+    >>> merge_iter([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge_iter([1, 2, 3], [])
+    [1, 2, 3]
+    >>> merge_iter([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    """
+    new = []
+    while lst1 and lst2:
+        if lst1[0] < lst2[0]:
+            new += [lst1[0]]
+            lst1 = lst1[1:]
+        else:
+            new += [lst2[0]]
+            lst2 = lst2[1:]
+    if lst1:
+        return new + lst1
+    else:
+        return new + lst2
+
+def mergesort(seq):
+    """Mergesort algorithm.
+
+    >>> mergesort([4, 2, 5, 2, 1])
+    [1, 2, 2, 4, 5]
+    >>> mergesort([])     # sorting an empty list
+    []
+    >>> mergesort([1])   # sorting a one-element list
+    [1]
+    """
+    # recursive solution
+    if len(seq) < 2:
+        return seq
+    mid = len(seq) // 2
+    return merge(mergesort(seq[:mid]), mergesort(seq[mid:]))
+
+# Iterative solution
+def mergesort_iter(seq):
+    """Mergesort algorithm.
+
+    >>> mergesort_iter([4, 2, 5, 2, 1])
+    [1, 2, 2, 4, 5]
+    >>> mergesort_iter([])     # sorting an empty list
+    []
+    >>> mergesort_iter([1])   # sorting a one-element list
+    [1]
+    """
+    if not seq:
+        return []
+    queue = [[elem] for elem in seq]
+    while len(queue) > 1:
+        first, second = queue[0], queue[1]
+        queue = queue[2:] + [merge(first, second)]
+    return queue[0]
